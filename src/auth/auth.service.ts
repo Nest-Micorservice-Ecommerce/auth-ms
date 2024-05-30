c559c7bf238cdf55aebb0d3599c8216d8b161ed0
Dieguidev <diegogaraycullas@gmail.com>
2024-05-30 18:46:35 -0500

    canectada la base de datos funcional el endpoint de crear usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { PrismaClient } from '@prisma/client';
+import { connect } from 'http2';
+import { RegisterUserDto } from './dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('Authservice')
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Authservice connected to database')
+  }
+
+  async registerUser(registerUserDto: RegisterUserDto) {
+
+    const { name, email, password } = registerUserDto;
+
+    try {
+      const user = await this.user.findUnique({
+        where: {
+          email
+        }
+      })
+
+      if (user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User already exists'
+        })
+      }
+      const newUser = await this.user.create({
+        data: {
+          name,
+          email,
+          password
+        }
+      })
+
+      return {
+        user: newUser
+      };
+
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message
+      })
+    }
+  }
+
+}
